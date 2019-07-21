b8c4453489f1ada09be16791d047b3c758495b01
dev-sincode <dhaval_singh@outlook.com>
2019-07-21 14:14:37 +0530

    outputing tweets in json file

--- bot.js
+++ bot.js
@@ -11,7 +11,6 @@ var stream = T.stream('user');
 stream.on('tweet',tweetEvent);
 
 function tweetEvent(eventMsg){
-    
     var json = JSON.stringify(eventMsg,null,2);
     fs.writeFile("tweet.json",json);
     console.log('hi');
